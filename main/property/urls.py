728c74efab038b84f84862e55927084f9aed8dfd
abu <abubakersuliman@outlook.com>
2023-01-21 19:50:34 +0400

    djangorestframework-simplejwt

--- main/property/urls.py
+++ main/property/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
 from property.domain.views import ProductSearchView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('search/', ProductSearchView.as_view(), name='product-search'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
