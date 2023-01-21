aac5d295b9d4275f57fe14da1df6b57d348f4732
abu <abubakersuliman@outlook.com>
2023-01-21 17:58:12 +0400

    check if exist

--- main/property/domain/views.py
+++ main/property/domain/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 import logging
 import redis
 from my_microservice import settings
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, generics
 from ..models import Product
 from rest_framework.filters import SearchFilter, OrderingFilter
 from ..filters import ProductFilter
@@ -18,36 +18,39 @@ redis_instance = redis.StrictRedis(
 logger = logging.getLogger(__name__)
 
 
-# class ProductSearchView(APIView):
-#     @swagger_auto_schema(query_serializer=ProductSearchSerializer)
-#     def get(self, request, format=None):
-#         serializer = ProductSearchSerializer(data=request.query_params)
-#         if serializer.is_valid():
-#             queryset = Product.objects.filter(
-#                 category=serializer.validated_data['category'],
-#                 brand=serializer.validated_data['brand'],
-#                 price__range=(serializer.validated_data['min_price'], serializer.validated_data['max_price']),
-#                 quantity__range=(serializer.validated_data['min_quantity'], serializer.validated_data['max_quantity']),
-#                 created_at=serializer.validated_data['created_at']
-#             )
-#             serializer = ProductSerializer(queryset, many=True)
-#             return Response(serializer.data)
-#         else:
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 class ProductSearchView(APIView):
     @swagger_auto_schema(query_serializer=ProductSearchSerializer)
     def get(self, request, format=None):
         serializer = ProductSearchSerializer(data=request.query_params)
         if serializer.is_valid():
-            queryset = Product.objects.filter(
-                category=serializer.validated_data['category'],
-                brand=serializer.validated_data['brand'],
-                rating=serializer.validated_data['rating'],
-                price__range=(serializer.validated_data['min_price'], serializer.validated_data['max_price']),
-                quantity__range=(serializer.validated_data['min_quantity'], serializer.validated_data['max_quantity']),
-                created_at=serializer.validated_data['created_at']
-            )
+            queryset = Product.objects.all()
+
+            category = serializer.validated_data.get('category')
+            if category:
+                queryset = queryset.filter(category=category)
+
+            brand = serializer.validated_data.get('brand')
+            if brand:
+                queryset = queryset.filter(brand=brand)
+
+            min_price = serializer.validated_data.get('min_price')
+            max_price = serializer.validated_data.get('max_price')
+            if min_price and max_price:
+                queryset = queryset.filter(price__range=(min_price, max_price))
+
+            min_quantity = serializer.validated_data.get('min_quantity')
+            max_quantity = serializer.validated_data.get('max_quantity')
+            if min_quantity and max_quantity:
+                queryset = queryset.filter(quantity__range=(min_quantity, max_quantity))
+
+            created_at = serializer.validated_data.get('created_at')
+            if created_at:
+                queryset = queryset.filter(created_at=created_at)
+
+            rating = serializer.validated_data.get('rating')
+            if rating:
+                queryset = queryset.filter(rating=rating)
+
             serializer = ProductSerializer(queryset, many=True)
             return Response(serializer.data)
         else:
