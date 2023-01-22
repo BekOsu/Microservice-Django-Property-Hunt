1b8e5d14c087b4ee8bb762bfc130d302b3bf808f
abu <abubakersuliman@outlook.com>
2023-01-22 10:49:32 +0400

    token-verify

--- main/property/urls.py
+++ main/property/urls.py
@@ -4,11 +4,12 @@ from property.domain.views import ProductSearchView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView
 )
 
 urlpatterns = [
     path('search/', ProductSearchView.as_view(), name='product-search'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
